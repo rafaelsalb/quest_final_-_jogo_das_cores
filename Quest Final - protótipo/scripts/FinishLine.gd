825ee5dd052a90ca09934762fa4d7c8cb6a7cdf5
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2023-05-16 10:55:57 -0300

    Delete Quest Final - protótipo directory

--- Quest Final - protótipo/scripts/FinishLine.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-export(int, "Menu", "Level 1", "Level 2", "Level 3") var NEXT_LEVEL
-
-
-func _on_FinishLine_body_entered(body):
-	if body.name == "Player":
-		Global.change_level(NEXT_LEVEL)
